1e3f5e81b8f94d5fee2ca7aa94a64700be0507f6
the-satyam <48691267+the-satyam@users.noreply.github.com>
2021-10-10 19:46:00 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,12 @@
+def solution(arr,n):
+  ans = 0
+  for el in range arr:
+    #
+  
+  return ans
+t = int(input())
+while t:
+  n = int(input())
+  arr = list(map(int,input().split()))
+  print(solution(arr,n)
+  t -=1
